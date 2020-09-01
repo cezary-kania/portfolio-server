68e0d7d887d877dea1b3e8e4eeb092b61d6eea14
Cezary <cezary@cezary-ubuntu20.4>
2020-09-01 16:48:17 +0200

    changed email credentials

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     #Mail config
     MAIL_SERVER = 'smtp.wp.pl'
     MAIL_PORT = 465
-    MAIL_DEFAULT_SENDER = 'flask_mailer@wp.pl'
-    MAIL_USERNAME = 'flask_mailer@wp.pl'
-    MAIL_PASSWORD = 'Qwerty1234'
+    MAIL_DEFAULT_SENDER = 'kania-halina@wp.pl'#'flask_mailer@wp.pl'
+    MAIL_USERNAME = 'kania-halina@wp.pl'#'flask_mailer@wp.pl'
+    MAIL_PASSWORD = 'kruszewianka1'#'Qwerty1234'
     MAIL_USE_SSL = True
 
 class ProdConfig(Config):
