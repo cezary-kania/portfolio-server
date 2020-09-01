70a7da9e03ad8923f74045dd9eb4a8412eabc8e5
Cezary <cezary@cezary-ubuntu20.4>
2020-09-01 17:45:39 +0200

    added cors

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_cors import CORS
+
 from config import DevConfig, ProdConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 
 api = Api(app)
+cors = CORS(app, resources={r"/*" : {"origins":"*"}})
 db = SQLAlchemy(app)
 mail = Mail(app)
 
